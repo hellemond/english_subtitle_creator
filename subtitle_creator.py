c7d64aaf33da67e5821d3565fb63b55802b0cb86
hellemond <hdargalaii@gmail.com>
2024-07-27 22:07:15 -0700

    imported deep_translator translators as well as added code to get user input for source language to translate from

--- subtitle_creator.py
+++ subtitle_creator.py
@@ -1,13 +1,25 @@
 #this code will take a video file of any language, and add english subtitles to it
 
 import assemblyai as aai
-
+from deep_translator import (GoogleTranslator,
+                             ChatGptTranslator,
+                             MicrosoftTranslator,
+                             PonsTranslator,
+                             LingueeTranslator,
+                             MyMemoryTranslator,
+                             YandexTranslator,
+                             PapagoTranslator,
+                             DeeplTranslator,
+                             QcriTranslator,
+                             single_detection,
+                             batch_detection)
 
 aai.settings.api_key = "66f9661530504430a3379b59f7a5f969"
-
+langs = langs_dict = GoogleTranslator().get_supported_languages(as_dict=True)
+checking_language = True
 
 def create_subtitles(path):
-    
+
     # subtitle generator config
     config = aai.TranscriptionConfig(speech_model=aai.SpeechModel.best,
                                      language_detection=True,
@@ -23,5 +35,33 @@ def create_subtitles(path):
     f.write(subtitles)
     f.close
 
+if __name__ == '__main__':
 
-create_subtitles('input.mp4')
+    print("\nType 'languages' for a list of available languages\n")
+
+    while checking_language == True:
+        #checks if language is within dictionary
+        source_lang = input("\nSource language of the video: ").lower()
+
+        for i in langs:
+            #checks through language dictionary to verify if input language is available to translate
+            if source_lang == i:
+                checking_language = False
+                lang_code = langs[source_lang]
+                print("\nLanguage available for translation.")
+                print(lang_code)
+                break
+        
+        if source_lang == 'languages':
+            #print list of available languages
+            for lang in langs:
+                print(lang)
+        else:
+            #retry code
+            if checking_language == False:
+                break
+            else:
+                print("\nThat language is not available, please try again.")
+    #manually input path to video needing subtitles
+    video_file = input("\nFile path of video to translate: ")
+    create_subtitles(video_file)
---
+++ test.py
@@ -0,0 +1,16 @@
+from deep_translator import (GoogleTranslator,
+                             ChatGptTranslator,
+                             MicrosoftTranslator,
+                             PonsTranslator,
+                             LingueeTranslator,
+                             MyMemoryTranslator,
+                             YandexTranslator,
+                             PapagoTranslator,
+                             DeeplTranslator,
+                             QcriTranslator,
+                             single_detection,
+                             batch_detection)
+
+langs = langs_dict = GoogleTranslator().get_supported_languages(as_dict=True)
+x = 'maithili'
+print(langs[x])
